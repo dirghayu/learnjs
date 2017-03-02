7e3dee13b6487ad0b2710d00d5c7ac692ee24a4c
dirghayu <dirghayu.kaninde@gmail.com>
2017-03-02 18:31:11 +1100

    Simple router defined

--- projects/public/app.js
+++ projects/public/app.js
@@ -1,6 +1,27 @@
 'use strict';
 var learnjs = {};
+
+learnjs.problemView = function(id){
+    return $('<div class="problem-view">').text('Problem #'+id+' Coming soon!');
+}
 learnjs.showView = function(hash){
-    var problemView = $('<div class="problem-view">').text('Coming soon');
-    ${'.view-container'}.empty().append(problemView)
+console.log(hash)
+    var route =     {
+        '#problem' : learnjs.problemView
+    }
+
+    var hashParts = hash.split('-');
+
+    var viewFn = route[hashParts[0]]
+    if(viewFn){
+    $('.view-container').empty().append(viewFn(hashParts[1]))
+    }
+
+}
+
+learnjs.appOnReady = function(){
+    window.onhashchange = function(){
+        learnjs.showView(window.location.hash);
+    }
+    learnjs.showView(window.location.hash);
 }
